231680ed2b1f93348562d7eea6e4592c8e2ad6f4
Andrew Gerst <gerst20051@gmail.com>
2012-06-01 00:06:29 -0400

    initial hns netai commit

---
+++ src/api/ajax.php
@@ -0,0 +1,227 @@
+<?php
+session_start();
+error_reporting(0);
+header('Access-Control-Allow-Origin: *');
+
+require_once 'functions.inc.php';
+require_once 'api.inc.php';
+require_once 'mysql.class.php';
+
+if (!$con = mysql_connect(MYSQL_HOST,MYSQL_USER,MYSQL_PASSWORD)) throw new Exception('Error connecting to the server');
+if (!mysql_select_db(MYSQL_DATABASE,$con)) throw new Exception('Error selecting database');
+
+switch ($_SERVER['REQUEST_METHOD']) {
+case 'POST': $_REQ = $_POST; break;
+case 'GET': $_REQ = $_GET; break;
+default: $_REQ = $_GET; break;
+}
+
+setglobal($_REQ);
+
+switch ($_SERVER['REQUEST_METHOD']) {
+case 'POST':
+if ($ACTION == 'login') {
+	$VARS = array_map('varcheck',$_REQ['form']);
+	if ($VARS['formname'] != 'login') print_json(array('logged'=>false),true);
+	else unset($VARS['formname']);
+	if (!validateinput($VARS,array('username','password'))) print_json(array('logged'=>false),true);
+	extract($VARS);
+	try {
+		$db = new MySQL();
+		$db->sfquery(array('SELECT * FROM login u JOIN info i ON u.user_id = i.user_id WHERE username = "%s" AND pass = PASSWORD("%s") LIMIT 1',$username,substr(base64_decode($password),3)));
+		if ($db->numRows() == 1) {
+			$row = $db->fetchAssocRow();
+			$_SESSION['logged'] = true;
+			$_SESSION['user_id'] = $row['user_id'];
+			$_SESSION['username'] = $row['username'];
+			$_SESSION['access_level'] = $row['access_level'];
+			$_SESSION['last_login'] = $row['last_login'];
+			if (varcheck($row['middlename'],true)) $_SESSION['fullname'] = $row['firstname'] . ' ' . $row['middlename'] . ' ' . $row['lastname'];
+			else $_SESSION['fullname'] = $row['firstname'] . ' ' . $row['lastname'];
+			$_SESSION['firstname'] = $row['firstname'];
+			if (varcheck($row['middlename'],true)) $_SESSION['middlename'] = $row['middlename'];
+			$_SESSION['lastname'] = $row['lastname'];
+			if (varcheck($row['images'],true)) $_SESSION['images'] = $row['images'];
+			
+
+			
+			print_json(array('logged'=>true),true);
+		} //else print_json(array('logged'=>false),false);
+			$data = "data";
+			$db->queryDebug('UPDATE stream SET likes = likes+1 WHERE sid = '.$data);
+			
+			
+			$db->queryDebug('UPDATE stream SET data = \''.mysql_real_escape_string(addslashes($data)).'\' WHERE sid = '.$data);
+	
+	
+			$db->queryDebug('UPDATE stream SET likes = likes+1, ids = \''.json_encode($data).'\' WHERE sid = '.$data);
+			
+			die();
+	} catch(Exception $e) {
+		echo $e->getMessage();
+		exit();
+	}
+} elseif ($ACTION == 'register') {
+	$VARS = array_map('varcheck',$_REQ['form']);
+	if ($VARS['formname'] != 'register') print_json(array('logged'=>false),true);
+	else unset($VARS['formname']);
+	$required = array('username','password','name','email','city','gender','bmonth','bday','byear');
+	if (!validateinput($VARS,$required)) print_json(array('logged'=>false),true);
+	extract($VARS);
+	if (empty($hometown)) $hometown = $city;
+	list($firstname, $middlename, $lastname) = split(' ',ucname($name));
+	if (!isset($lastname)) { $lastname = $middlename; $middlename = ''; }
+	try {
+		$db = new MySQL();
+		$db->insert('login', array(
+			'username'=>$username,
+			'access_level'=>1,
+			'last_login'=>date('Y-m-d'),
+			'date_joined'=>date('Y-m-d'),
+			'logins'=>1
+		));
+		$user_id = $db->insertID();
+		$db->query('UPDATE login SET pass = PASSWORD("'.mysql_real_escape_string(substr(base64_decode($password),3)).'") WHERE user_id = '.$user_id);
+		$db->insert('info', array(
+			'user_id'=>$user_id,
+			'firstname'=>$firstname,
+			'middlename'=>$middlename,
+			'lastname'=>$lastname,
+			'email'=>$email,
+			'gender'=>$gender,
+			'hometown'=>$hometown,
+			'current_city'=>$city,
+			'birth_month'=>$bmonth,
+			'birth_day'=>$bday,
+			'birth_year'=>$byear
+		));
+		if ($db->affectedRows() == 1) {
+			$_SESSION['logged'] = true;
+			$_SESSION['user_id'] = $user_id;
+			$_SESSION['username'] = $username;
+			$_SESSION['access_level'] = 1;
+			$_SESSION['last_login'] = date('Y-m-d');
+			if (varcheck($middlename,true)) {
+				$_SESSION['fullname'] = $firstname . ' ' . $middlename . ' ' . $lastname;
+				$_SESSION['middlename'] = $middlename;
+			} else $_SESSION['fullname'] = $firstname . ' ' . $lastname;
+			$_SESSION['firstname'] = $firstname;
+			$_SESSION['lastname'] = $lastname;
+			print_json(array('logged'=>true),true);
+		} else print_json(array('logged'=>false),true);
+	} catch(Exception $e) {
+		echo $e->getMessage();
+		exit();
+	}
+} elseif ($ACTION == 'logout') {
+	logout();
+	print_json(array('logged'=>false),true);
+}
+break;
+case 'GET':
+if ($ACTION == 'logged') {
+	if (isset($_SESSION['logged'])) print_json(array('logged'=>true),true);
+	else print_json(array('logged'=>false),true);
+} elseif ($ACTION == 'hnsuser') {
+?>
+<div class="lightbox">
+<div id="hnsuser" class="modal">
+<div id="login">
+<header>Login using Homenet Spaces</header>
+<form id="f_login" onsubmit="return false">
+<input type="hidden" name="formname" value="login"/>
+<div><label for="lusername">Username:</label><input id="lusername" name="username" type="text" value=""/></div>
+<div><label for="lpassword">Password:</label><input id="lpassword" name="password" type="password" value=""/></div>
+<div class="buttonTools">
+<ul class="toolList rfloat">
+<li class="listItem"><label class="loginButton" for="b_login_splash"><input id="b_login_splash" value="Login" type="submit"/></label></li>
+<li class="listItem"><label class="registerButton" for="b_register_splash"><input id="b_register_splash" value="Register" type="button"/></label></li>
+</ul>
+</div>
+</form>
+</div>
+<div id="register">
+<header>Register for Homenet Spaces</header>
+<form id="f_register" onsubmit="return false">
+<input type="hidden" name="formname" value="register"/>
+<div><label for="reg_username">Username:</label><input id="reg_username" name="username" type="text" value=""/></div>
+<div><label for="reg_password">Password:</label><input id="reg_password" name="password" type="password" value=""/></div>
+<div><label for="reg_name">Full Name:</label><input id="reg_name" name="name" type="text" value=""/></div>
+<div><label for="reg_email">Email:</label><input id="reg_email" name="email" type="email" value=""/></div>
+<div><label for="reg_city">Current City:</label><input id="reg_city" name="city" type="text" value=""/></div>
+<div><label for="reg_hometown">Hometown:</label><input id="reg_hometown" name="hometown" type="text" value=""/></div>
+<div>
+<label for="reg_gender">Gender:</label>
+<select id="reg_gender" name="gender">
+<option value="0"></option>
+<option value="male">Male</option>
+<option value="female">Female</option>
+</select> 
+</div>
+<div>
+<label for="reg_bmonth">Birth Date:</label>
+<select id="reg_bmonth" name="bmonth">
+<option value="0"></option>
+<option value="1">January</option>
+<option value="2">February</option>
+<option value="3">March</option>
+<option value="4">April</option>
+<option value="5">May</option>
+<option value="6">June</option>
+<option value="7">July</option>
+<option value="8">August</option>
+<option value="9">September</option>
+<option value="10">October</option>
+<option value="11">November</option>
+<option value="12">December</option>
+</select>
+<select id="reg_bday" name="bday"><option value="0"></option></select>
+<select id="reg_byear" name="byear"><option value="0"></option></select>
+</div>
+<div class="buttonTools">
+<ul class="toolList lfloat">
+<li class="listItem"><label class="loginButton" for="b_login"><input id="b_login" value="Login" type="submit"/></label></li>
+<li class="listItem"><label class="registerButton" for="b_register"><input id="b_register" value="Register" type="submit"/></label></li>
+</ul>
+</div>
+</form>
+</div>
+<script>
+(function(){
+var bday="",byear="",date=new Date();
+for (i=1;i<=31;i++){bday+="<option value=\""+i+"\">"+i+"</option>";}
+for (i=date.getFullYear();i>=1902;i--){byear+="<option value=\""+i+"\">"+i+"</option>";}
+$("#reg_bday").append(bday);
+$("#reg_byear").append(byear);
+})();
+</script>
+</div>
+</div>
+<?php
+} elseif ($ACTION == 'username') {
+	try {
+		$db = new MySQL();
+		$db->query('SELECT username FROM login WHERE username = "'.$USERNAME.'" LIMIT 1');
+		if ($db->numRows() == 1) print_json(array('user'=>true),true);
+		else print_json(array('user'=>false),true);
+	} catch(Exception $e) {
+		echo $e->getMessage();
+		exit();
+	}
+} elseif ($ACTION == "userdata") {
+	varcheck($UID,true,$_SESSION['user_id'],"uid");
+	try {
+		$db = new MySQL();
+		$db->query('SELECT u.user_id,u.username,i.firstname,i.middlename,i.lastname,i.images FROM (login u JOIN info i ON u.user_id = i.user_id) WHERE u.user_id = '.$UID.' LIMIT 1');
+		if ($db->numRows() == 1) {
+			header('Content-Type: application/json; charset=utf8');
+			print_json(array('user'=>$db->fetchAssocRow()));
+		} else print_json(array('user'=>false),true);
+	} catch(Exception $e) {
+		echo $e->getMessage();
+		exit();
+	}
+}
+break;
+}
+?>
---
+++ src/api/script.js
@@ -0,0 +1,186 @@
+$w.$&&main()||function(){var a=$d.createElement("script");a.setAttribute("type","text/javascript");a.setAttribute("src","jquery.js");a.onload=main;a.onreadystatechange=function(){if(this.readyState=="complete"||this.readyState=="loaded")main()};($d.getElementsByTagName("head")[0]||$d.documentElement).appendChild(a)}();
+
+function main(){
+$w.aC = {
+title: "HnS User Dashboard",
+ajaxurl: $host+"ajax.php",
+apikey: "hnsapi",
+logged: false,
+loginFocus: false,
+registerFocus: false,
+user: {},
+init: function(){
+	$.getJSON(aC.ajaxurl, {action:"logged",apikey:aC.apikey}, function(response){
+		if (response.logged === true) aC.logged = true;
+		if (aC.logged === true) aC.loggedIn(); else aC.loggedOut();
+	});
+	aC.dom();
+},
+loggedIn: function(){
+	if (aC.logged === false) return;
+	$.getJSON(aC.ajaxurl, {action:"userdata",apikey:aC.apikey}, function(response){
+		if (response.user !== false) {
+			aC.user = response.user;
+			if (aC.user.middlename != "") aC.user.fullname = aC.user.firstname+' '+aC.user.middlename+' '+aC.user.lastname;
+			else aC.user.fullname = aC.user.firstname+' '+aC.user.lastname;
+			$($d.body).html('<div>Welcome '+aC.user.fullname+' | <span class="logout-link link">Logout</span></div>');
+		} else aC.logout();
+	});
+},
+loggedOut: function(){
+	$.get(aC.ajaxurl, {action:"hnsuser",apikey:aC.apikey}, function(response){
+		$($d.body).append(response).find('#hnsuser').center().parent().hide().css('visibility','visible').fadeIn('slow');
+	});
+},
+login: function(){
+	var e = false, username = $("#lusername"), password = $("#lpassword");
+	if ($.trim(username.val()) == "") { username.addClass('error'); e = true; } else username.removeClass('error');
+	if ($.trim(password.val()) == "") { password.addClass('error'); e = true; } else password.removeClass('error');
+	if (!e) {
+		$("#f_login").find("input,textarea,select,:radio").attr('disabled',true);
+		var output = {}, inputs = $("#f_login").find("input").filter("[name]");
+		$.map(inputs, function(n, i){
+			output[n.name] = $.trim($(n).val());
+		});
+		output.password = secure('hns'+output.password);
+		$.post(aC.ajaxurl, {action:"login",form:output,apikey:aC.apikey}, function(response){
+			if (stringToBoolean(response.logged)) aC.logged = true;
+			if (aC.logged === false) $("#f_login").find("input,textarea,select,:radio").attr('disabled',false);
+			else aC.loggedIn();
+		});
+	}
+},
+logout: function(){
+	$.post(aC.ajaxurl, {action:"logout",apikey:aC.apikey}, function(response){
+		if (!stringToBoolean(response.logged)) {
+			aC.logged = false;
+			aC.user = {};
+			aC.loggedOut();
+		}
+	});
+},
+regValidate: function(){
+	var e = false,
+	username = $("#reg_username"), username_trim = $.trim(username.val()),
+	password = $("#reg_password"), password_trim = $.trim(password.val()),
+	name = $("#reg_name"), name_trim = $.trim(name.val()),
+	email = $("#reg_email"), email_trim = $.trim(email.val()),
+	city = $("#reg_city"), city_trim = $.trim(city.val()),
+	bmonth = $("#reg_bmonth"),
+	bday = $("#reg_bday"),
+	byear = $("#reg_byear"),
+	usernameReg = /\W/,
+	nameReg = /[A-Za-z'-]/,
+	emailReg = /^[^0-9][a-zA-Z0-9_]+([.][a-zA-Z0-9_]+)*[@][a-zA-Z0-9_]+([.][a-zA-Z0-9_]+)*[.][a-zA-Z]{2,4}?$/i;
+
+	if (username_trim == "") { username.addClass('error'); e = true; }
+	else if (usernameReg.test(username_trim)) { username.addClass('error'); e = true; }
+	else { aC.checkUsername(username_trim); if (username.hasClass('error')) e = true; }
+	if (password_trim == "") { password.addClass('error'); e = true; } else password.removeClass('error');
+
+	if (name_trim == "") { name.addClass('error'); e = true; }
+	else if (!nameReg.test(name_trim)) { name.addClass('error'); e = true; }
+	else if (name_trim.split(' ').length < 2) { name.addClass('error'); e = true; } else name.removeClass('error');
+
+	if (email_trim == "") { email.addClass('error'); e = true; }
+	else if (!emailReg.test(email_trim)) { email.addClass('error'); e = true; } else email.removeClass('error');
+
+	if (city_trim == "") { city.addClass('error'); e = true; } else city.removeClass('error');
+	if (bmonth.val() == 0) { bmonth.addClass('error'); e = true; } else bmonth.removeClass('error');
+	if (bday.val() == 0) { bday.addClass('error'); e = true; } else bday.removeClass('error');
+	if (byear.val() == 0) { byear.addClass('error'); e = true; } else byear.removeClass('error');
+	
+	return !e;
+},
+getLocation: function(query){
+	var location = "37.76999,-122.44696";
+	if (navigator.geolocation) {
+		navigator.geolocation.getCurrentPosition(function(position){
+			var lat = position.coords.latitude;
+			var lng = position.coords.longitude;
+			location = lat+","+lng;
+		}, function(error){
+			log("geolocation error: "+error.message);
+		}, {maximumAge:60000, timeout:5000, enableHighAccuracy:true});
+	}
+	try {
+		$.getJSON("https://maps.googleapis.com/maps/api/place/autocomplete/json", {input:encodeURIComponent(query),location:location,sensor:false,key:"AIzaSyB4AY9CTv5F7wUzhN-FJgIpWWZfbU0AEjM"}, function(response){
+			log(response);
+		});
+	} catch(e) {
+		log(e);
+	}
+},
+checkUsername: function(uname){
+	uname = $.trim(uname);
+	if (uname != "") {
+		$.get(aC.ajaxurl, {action:"username",username:uname,apikey:aC.apikey}, function(response){
+			if (stringToBoolean(response.user)) $("#reg_username").addClass('error');
+			else $("#reg_username").removeClass('error');
+		});
+	} else $("#reg_username").addClass('error');
+},
+onKeyDown: function(e){
+	var keyCode = e.keyCode || e.which;
+	if (aC.logged === false) {
+		if (keyCode == keys.ENTER) {
+			if (aC.loginFocus) $("#b_login_splash").click();
+			else if (aC.registerFocus) $("#b_register").click();
+		}
+	}
+},
+dom: function(){
+	$("#lusername, #lpassword").live('focus',function(){
+		aC.loginFocus = true;
+	}).live('blur',function(){
+		aC.loginFocus = false;
+	});
+	$("#b_login_splash").live('click',function(){
+		aC.login();
+	});
+	$("#b_register_splash").live('click',function(){
+		$("#register").show();
+		$("#login").hide();
+		$("#hnsuser").center();
+	});
+	$("#reg_username, #reg_password, #reg_name, #reg_email, #reg_hometown, #reg_city").live('focus',function(){
+		aC.registerFocus = true;
+	}).live('blur',function(){
+		aC.registerFocus = false;
+	});
+	$("#reg_username").live('blur',function(){
+		aC.checkUsername(this.value);
+	});
+	$("#reg_hometown").live('keyup',function(){
+		aC.getLocation(this.value);
+	});
+	$("#b_register").live('click',function(){
+		if (!aC.regValidate()) return;
+		$("#f_register").find("input,textarea,select,:radio").attr('disabled',true);
+		var output = {}, inputs = $("#f_register").find("input,textarea,select,:radio").filter("[name]");
+		$.map(inputs, function(n, i){
+			output[n.name] = $.trim($(n).val());
+		});
+		output.password = secure('hns'+output.password);
+		$.post(aC.ajaxurl, {action:"register",form:output,apikey:aC.apikey}, function(response){
+			if (stringToBoolean(response.logged)) {
+				aC.logged = true; aC.loggedIn();
+			} else $("#f_register").find("input,textarea,select,:radio").attr('disabled',false);
+		});
+	});
+	$("#b_login").live('click',function(){
+		$("#login").show();
+		$("#register").hide();
+		$("#hnsuser").center();
+	});
+	$(".logout-link").live('click',function(){
+		aC.logout();
+	});
+}
+};
+
+$($d.documentElement).keydown($w.aC.onKeyDown);
+$($d).ready($w.aC.init);
+
+return true;
+}
