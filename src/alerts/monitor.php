7841d53850c7f44032be6bfcf44072f4cccf9c83
Andrew Gerst <gerst20051@gmail.com>
2012-06-15 14:08:05 -0400

    removed cron job comments

--- src/alerts/monitor.php
+++ src/alerts/monitor.php
@@ -22,16 +22,4 @@ function alert($host) {
 $host = 'www.catswhoblog.com';
 $find = 'Cats Who Code';
 if (!check($host, $find)) alert($host);
-
-// cron job to run every hour
-// 0 * * * * /usr/local/bin/php -q /htdocs/www/monitor.php
-/*
-Some valid examples of cron times:
-0 0 * * * - will run script every midnight 0:00 AM
-0 * * * * - will run script every hour
-15 * * * * - will run script every 15 minutes
-5 8 * * * - will run script every day 5 minutes past 8 AM
-5 8 15 * * - will run script every month day 15, 5 minutes past 8 AM
-5 8 * * 1 - will run script Monday only, 5 minutes past 8 AM
-*/
 ?>
