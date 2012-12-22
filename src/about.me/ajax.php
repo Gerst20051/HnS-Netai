19252372b2bf2efa24fad0d19324039a291ab5cb
Andrew Gerst <gerst20051@gmail.com>
2012-12-22 09:16:24 -0500

    initial commit of about.me

---
+++ src/about.me/ajax.php
@@ -0,0 +1,203 @@
+<?php
+session_start();
+
+require_once 'config.inc.php';
+require_once 'functions.inc.php';
+require_once 'mysql.class.php';
+
+if (!$con = mysql_connect(MYSQL_HOST,MYSQL_USER,MYSQL_PASSWORD)) throw new Exception('Error connecting to the server');
+if (!mysql_select_db(MYSQL_DATABASE,$con)) throw new Exception('Error selecting database');
+
+switch ($_SERVER['REQUEST_METHOD']) {
+case 'POST': $_REQ = $_POST; break;
+case 'GET': $_REQ = $_GET; break;
+default: $_REQ = $_GET; break;
+}
+
+setglobal($_REQ);
+
+switch ($_SERVER['REQUEST_METHOD']) {
+case 'POST':
+if ($ACTION == 'login') {
+	require_once 'encryption.inc.php';
+	$VARS = array_map('varcheck',$FORM);
+	if ($VARS['formname'] != 'login') print_json(array('logged'=>false));
+	else unset($VARS['formname']);
+	if (!validateinput($VARS,array('email','password'))) print_json(array('logged'=>false));
+	extract($VARS);
+	try {
+		$db = new MySQL();
+		$db->sfquery(array('SELECT salt FROM `%s` WHERE email = "%s" LIMIT 1',MYSQL_TABLE,$email));
+		if ($db->numRows()) {
+			$result = $db->fetchAssocRow();
+			$salt = $result['salt'];
+			$secure_password = pbkdf2($password,$salt);
+			$db->sfquery(array('SELECT %s FROM `%s` WHERE email = "%s" AND pass = "%s" LIMIT 1',MYSQL_ALL,MYSQL_TABLE,$email,$secure_password));
+			if ($db->numRows()) {
+				$row = $db->fetchAssocRow();
+				$_SESSION['logged'] = true;
+				$_SESSION['user_id'] = $row['user_id'];
+				$_SESSION['email'] = $row['email'];
+				$_SESSION['pageurl'] = $row['pageurl'];
+				$_SESSION['fullname'] = $row['firstname'] . ' ' . $row['lastname'];
+				$_SESSION['firstname'] = $row['firstname'];
+				$_SESSION['lastname'] = $row['lastname'];
+				$_SESSION['access_level'] = $row['access_level'];
+				$_SESSION['last_login'] = $row['last_login'];
+				$last_login = time();
+				$logins = $row['logins']+1;
+				$db->sfquery(array('UPDATE `%s` SET last_login = "%s", logins = "%s" WHERE user_id = %s',MYSQL_TABLE,$last_login,$logins,$_SESSION['user_id']));
+				print_json(array('logged'=>true));
+			} else print_json(array('logged'=>false));
+		} else print_json(array('logged'=>false));
+	} catch(Exception $e) {
+		echo $e->getMessage();
+		exit();
+	}
+} elseif ($ACTION == 'register') {
+	require_once 'encryption.inc.php';
+	$VARS = array_map('varcheck',$FORM);
+	if ($VARS['formname'] != 'register') print_json(array('registered'=>false));
+	else unset($VARS['formname']);
+	$required = array('name','email','password','pageurl');
+	if (!validateinput($VARS,$required)) print_json(array('registered'=>false));
+	extract($VARS);
+	list($firstname, $lastname) = split(' ',ucname($name));
+	$salt = genSalt();
+	$secure_password = pbkdf2($password,$salt);
+	$timestamp = time();
+	try {
+		$db = new MySQL();
+		$db->insert(MYSQL_TABLE, array(
+			'email'=>$email,
+			'pass'=>$secure_password,
+			'salt'=>$salt,
+			'pageurl'=>$pageurl,
+			'firstname'=>$firstname,
+			'lastname'=>$lastname,
+			'access_level'=>1,
+			'last_login'=>$timestamp,
+			'date_joined'=>$timestamp,
+			'logins'=>1
+		));
+		if ($db->affectedRows()) {
+			$user_id = $db->insertID();
+			$_SESSION['logged'] = true;
+			$_SESSION['user_id'] = $user_id;
+			$_SESSION['email'] = $email;
+			$_SESSION['pageurl'] = $pageurl;
+			$_SESSION['fullname'] = $firstname . ' ' . $lastname;
+			$_SESSION['firstname'] = $firstname;
+			$_SESSION['lastname'] = $lastname;
+			$_SESSION['access_level'] = 1;
+			$_SESSION['last_login'] = $timestamp;
+			print_json(array('registered'=>true));
+		} else print_json(array('registered'=>false));
+	} catch(Exception $e) {
+		echo $e->getMessage();
+		exit();
+	}
+} elseif ($ACTION == 'logout') {
+	logout();
+	print_json(array('logged'=>false));
+} elseif ($ACTION == 'addpage') {
+	$VARS = array_map('varcheck',$FORM);
+	if ($VARS['formname'] != 'addpage') print_json(array('added'=>false));
+	else unset($VARS['formname']);
+	$required = array('name','url');
+	if (!validateinput($VARS,$required)) print_json(array('added'=>false));
+	extract($VARS);
+	$page = array("name"=>$name,"url"=>$url);
+	try {
+		$db = new MySQL();
+		$db->sfquery(array('SELECT pages FROM `%s` WHERE user_id = %s LIMIT 1',MYSQL_TABLE,$_SESSION['user_id']));
+		if ($db->numRows()) {
+			$row = $db->fetchAssocRow();
+			$pages = json_decode($row['pages']);
+			array_push($pages,$page)
+			$db->sfquery(array('UPDATE `%s` SET pages = "%s" WHERE user_id = %s',MYSQL_TABLE,$pages,$_SESSION['user_id']));
+			print_json(array('added'=>true));
+		} else print_json(array('added'=>false));
+	} catch(Exception $e) {
+		echo $e->getMessage();
+		exit();
+	}
+} elseif ($ACTION == 'deletepage') {
+	try {
+		$db = new MySQL();
+		$db->sfquery(array('SELECT pages FROM `%s` WHERE user_id = %s LIMIT 1',MYSQL_TABLE,$_SESSION['user_id']));
+		if ($db->numRows()) {
+			$row = $db->fetchAssocRow();
+			$pages = json_decode($row['pages']);
+			$original_amount
+			array_splice($pages, $index, 1);
+			$db->sfquery(array('UPDATE `%s` SET pages = "%s" WHERE user_id = %s',MYSQL_TABLE,$pages,$_SESSION['user_id']));
+			if () {
+				print_json(array('deleted'=>true));
+			}
+		} else print_json(array('deleted'=>false));
+	} catch(Exception $e) {
+		echo $e->getMessage();
+		exit();
+	}
+} elseif ($ACTION == 'updatepage') {
+	try {
+		$db = new MySQL();
+		$db->sfquery(array('SELECT pages FROM `%s` WHERE user_id = %s LIMIT 1',MYSQL_TABLE,$_SESSION['user_id']));
+		if ($db->numRows()) {
+			$row = $db->fetchAssocRow();
+			$pages = json_decode($row['pages']);
+			$data = $row;
+			$db->sfquery(array('UPDATE `%s` SET pages = "%s" WHERE user_id = %s',MYSQL_TABLE,$data,$_SESSION['user_id']));
+			print_json(array('updated'=>true));
+		} else print_json(array('updated'=>false));
+	} catch(Exception $e) {
+		echo $e->getMessage();
+		exit();
+	}
+}
+break;
+case 'GET':
+if ($ACTION == 'logged') {
+	if (isset($_SESSION['logged'])) print_json(array('logged'=>true));
+	else print_json(array('logged'=>false));
+} elseif ($ACTION == 'checkemail') {
+	try {
+		$db = new MySQL();
+		$db->sfquery(array('SELECT email FROM `%s` WHERE email = "%s" LIMIT 1',MYSQL_TABLE,$EMAIL));
+		if ($db->numRows()) print_json(array('email'=>true));
+		else print_json(array('email'=>false));
+	} catch(Exception $e) {
+		echo $e->getMessage();
+		exit();
+	}
+} elseif ($ACTION == 'userdata') {
+	try {
+		$db = new MySQL();
+		$db->sfquery(array('SELECT %s FROM `%s` WHERE user_id = %s LIMIT 1',MYSQL_ALL,MYSQL_TABLE,$_SESSION['user_id']));
+		if ($db->numRows()) {
+			$data = $db->fetchParsedRow();
+			print_json(array('user'=>$data));
+		} else print_json(array('user'=>false));
+	} catch(Exception $e) {
+		echo $e->getMessage();
+		exit();
+	}
+} elseif ($ACTION == 'profile') {
+	try {
+		$db = new MySQL();
+		$db->sfquery(array('SELECT %s FROM `%s` WHERE pageurl = "%s" LIMIT 1',MYSQL_ALL,MYSQL_TABLE,$PAGEURL));
+		if ($db->numRows()) {
+			$data = $db->fetchParsedRow();
+			print_json(array('user'=>$data));
+		} else print_json(array('user'=>false));
+	} catch(Exception $e) {
+		echo $e->getMessage();
+		exit();
+	}
+}
+break;
+}
+
+exit();
+?>
---
+++ src/about.me/script.js
@@ -0,0 +1,280 @@
+(function(window, document, $, undefined){
+($ && main($) && jQueryPlugins()) || !function(){
+	var s = document.createElement("script"), h = document.head || document.getElementsByTagName("head")[0] || document.documentElement, done = false;
+	s.src = "jquery.js";
+	s.onload = s.onreadystatechange = function(){
+		if (!this.readyState || this.readyState == "complete" || this.readyState == "loaded") {
+			if (!done && (done=true)) {
+				main(window.jQuery) && jQueryPlugins();
+				s.onload = s.onreadystatechange = null;
+				if (h && s.parentNode) h.removeChild(s);
+				s = undefined;
+			}
+		}
+	};
+	h.insertBefore(s, h.firstChild);
+}();
+
+function main($){
+if (main.run) return;
+else main.run = true;
+
+window.aC = {
+title: "about.me",
+ajaxurl: "ajax.php",
+loaded: false,
+logged: false,
+loginFocus: false,
+registerFocus: false,
+user: {},
+page: {},
+handleHash: function(){
+	if (Hash.getHash().length) {
+		Hash.parse();
+		if (Hash.has("who")) {
+			var who = Hash.get("who");
+			this.loadProfile(who);
+		}
+	}
+},
+init: function(){
+	if (this.loaded !== false) return;
+	var self = this;
+	$.getJSON(this.ajaxurl, {action:"logged"}, function(response){
+		self.loaded = true;
+		if (response.logged === true) {
+			self.logged = true;
+			self.loggedIn();
+		} else self.loggedOut();
+		self.handleHash();
+	});
+	this.dom();
+},
+loggedIn: function(){
+	if (this.logged !== true) return;
+	var self = this;
+	$.getJSON(this.ajaxurl, {action:"userdata"}, function(response){
+		if (response.user !== false) {
+			self.user = response.user;
+			self.user.fullname = self.user.firstname+' '+self.user.lastname;
+			$("#loggedin").show();
+			$("#loggedout").hide();
+			$("body").addClass("in").removeClass("out");
+			self.handleHash();
+			self.displayMe();
+		} else self.logout();
+	});
+},
+loggedOut: function(){
+	if (this.logged !== false) return;
+	$("#loggedout").show();
+	$("#loggedin").hide();
+	$("body").addClass("out").removeClass("in");
+},
+login: function(){
+	var self = this, e = false, $login = $("#f_login"), $email = $login.find("#lemail"), $password = $login.find("#lpassword");
+	if (!$.trim($email.val()).length) { $email.addClass('error'); e = true; } else $email.removeClass('error');
+	if (!$.trim($password.val()).length) { $password.addClass('error'); e = true; } else $password.removeClass('error');
+	if (!e) {
+		$login.find("input").attr('disabled',true);
+		var output = {}, inputs = $login.find("input").filter("[name]");
+		$.map(inputs, function(n, i){
+			output[n.name] = $.trim($(n).val());
+		});
+		$.post(this.ajaxurl, {action:"login",form:output}, function(response){
+			$login.find("input").attr('disabled',false);
+			if (stringToBoolean(response.logged)) {
+				$login.find("#reg_name, #reg_email, #reg_password").removeClass('error');
+				$login.find("#b_login_splash").removeClass('error');
+				$("#f_register").clearForm();
+				$login.clearForm();
+				self.logged = true;
+				self.loggedIn();
+			} else {
+				$login.find("#b_login_splash").addClass('error');
+				$password.val('');
+			}
+		});
+	}
+},
+logout: function(){
+	var self = this;
+	$.post(this.ajaxurl, {action:"logout"}, function(response){
+		if (!stringToBoolean(response.logged)) {
+			self.logged = false;
+			self.user = {};
+			self.page = {};
+			self.loggedOut();
+			Hash.clear();
+		}
+	});
+},
+regValidate: function(){
+	var e = false,
+	$reg = $("#f_register"),
+	$name = $reg.find("#reg_name"), name_trim = $.trim($name.val()),
+	$email = $reg.find("#reg_email"), email_trim = $.trim($email.val()),
+	$password = $reg.find("#reg_password"), password_trim = $.trim($password.val()),
+	$pageurl = $reg.find("#reg_pageurl"), pageurl_trim = $.trim($pageurl.val()),
+	nameReg = /[A-Za-z'-]/,
+	emailReg = /^[^0-9][a-zA-Z0-9_]+([.][a-zA-Z0-9_]+)*[@][a-zA-Z0-9_]+([.][a-zA-Z0-9_]+)*[.][a-zA-Z]{2,4}?$/i,
+	pageurlReg = /[A-Za-z0-9-_]/;
+
+	if (!name_trim.length || !nameReg.test(name_trim) || name_trim.split(' ').length < 2) {
+		$name.addClass('error');
+		e = true;
+	} else $name.removeClass('error');
+
+	if (!email_trim.length || !emailReg.test(email_trim) || this.isEmailRegistered(email_trim)) {
+		$email.addClass('error');
+		e = true;
+	} else $email.removeClass('error');
+	
+	if (!password_trim.length) {
+		$password.addClass('error');
+		e = true;
+	} else $password.removeClass('error');
+
+	if (!pageurl_trim.length || !pageurlReg.test(pageurl_trim)) {
+		$pageurl.addClass('error');
+		e = true;
+	} else $pageurl.removeClass('error');
+	
+	return !e;
+},
+register: function(){
+	if (!this.regValidate()) return;
+	var self = this, output = {}, $f_register = $("#f_register"), inputs = $f_register.find("input").filter("[name]");
+	$f_register.find("input").attr('disabled',true);
+	$.map(inputs, function(n, i){
+		output[n.name] = $.trim($(n).val());
+	});
+	$.post(this.ajaxurl, {action:"register",form:output}, function(response){
+		$f_register.find("input").attr('disabled',false);
+		if (stringToBoolean(response.registered)) {
+			$f_register.find("#b_register").removeClass('error');
+			$f_register.clearForm();
+			self.logged = true;
+			self.registered();
+		} else {
+			$f_register.find("#b_register").addClass('error');
+		}
+	});
+},
+registered: function(){
+	this.loggedIn();
+},
+isEmailRegistered: function(email){
+	email = $.trim(email);
+	if (email.length) {
+		$.get(this.ajaxurl, {action:"checkemail",email:email}, function(response){
+			if (stringToBoolean(response.email)) return true;
+			else return false;
+		});
+	}
+},
+onKeyDown: function(e){
+	var keyCode = e.which;
+	if (this.logged === false) {
+		if (keyCode == keys.ENTER) {
+			e.preventDefault();
+			if (this.loginFocus) this.login();
+			else if (this.registerFocus) this.register();
+		}
+	}
+},
+displayMe: function(pageurl){
+	var pages = "";
+	$("#profilename").text(this.user.fullname);
+	$.each(this.user.pages, function(i,v){
+		pages += $("</span>").text("hey");
+	});
+	$("#profilelinklist").html(pages);
+},
+loadProfile: function(pageurl){
+	var self = this;
+	$.getJSON(this.ajaxurl, {action:"profile",pageurl:pageurl}, function(response){
+		if (response.user !== false) {
+			self.page = response.user;
+			self.page.fullname = self.page.firstname+' '+self.page.lastname;
+			if (this.logged === false) {
+				$("#previewprofile_name").text(response.page.fullname);
+				$("#splash").hide();
+				$("#previewprofile").show();
+			} else {
+
+			}
+		}
+	});
+},
+addPage: function(){
+	var self = this, e = false, $f_addpage = $("#f_addpost"), $po = f_addpage.find("#lemail"), $password = f_addpage.find("#lpassword");
+	if (!$.trim($email.val()).length) { $email.addClass('error'); e = true; } else $email.removeClass('error');
+	if (!$.trim($password.val()).length) { $password.addClass('error'); e = true; } else $password.removeClass('error');
+	if (!e) {
+		f_addpage.find("input").attr('disabled',true);
+		var output = {}, inputs = f_addpage.find("input").filter("[name]");
+		$.map(inputs, function(n, i){
+			output[n.name] = $.trim($(n).val());
+		});
+		$.post(this.ajaxurl, {action:"addpage",form:output}, function(response){
+			f_addpage.find("input").attr('disabled',false);
+			if (stringToBoolean(response.logged)) {
+				f_addpage.find("#reg_name, #reg_email, #reg_password").removeClass('error');
+				f_addpage.find("#b_login_splash").removeClass('error');
+				f_addpage.clearForm();
+			} else {
+				f_addpage.find("#b_login_splash").addClass('error');
+			}
+		});
+	}
+},
+dom: function(){
+	var self = this;
+	$("#authpanel").on({
+		focus: function(){
+			self.loginFocus = true;
+		},
+		blur: function(){
+			self.loginFocus = false;
+		}
+	},'#lemail, #lpassword');
+	$("#authpanel").on({
+		focus: function(){
+			self.registerFocus = true;
+		},
+		blur: function(){
+			self.registerFocus = false;
+			self.regValidate();
+		}
+	},'#reg_name, #reg_email, #reg_password, #reg_pageurl');
+	$("#authpanel").on('click','#b_login_splash',function(){
+		self.login();
+	});
+	$("#authpanel").on('click','#b_register_splash',function(){
+		$("#register").show();
+		$("#login").hide();
+	});
+	$("#authpanel").on('click','#b_register',function(){
+		self.register();
+	});
+	$("#authpanel").on('click','#b_login',function(){
+		$("#login").show();
+		$("#register").hide();
+	});
+	$("#hd_home").on('click','#homelink', function(){
+		$("#previewprofile").hide();
+		$("#splash").show();
+	});
+	$("#loggedin").on('click','.logout-link', function(){
+		self.logout();
+	});
+}
+};
+
+$(document.documentElement).keydown(bind(aC,aC.onKeyDown));
+$(document).ready(bind(aC,aC.init));
+
+return true;
+}
+})(this, this.document, this.jQuery);
