1dfcbcdbe16dae903c787216715319c6f50a1aa8
Andrew Gerst <gerst20051@gmail.com>
2014-01-11 13:43:36 -0500

    added config files

--- src/contacts/mysql.config.backup.php
+++ src/contacts/mysql.config.backup.php
@@ -3,17 +3,15 @@ require_once 'config.inc.php';
 
 if (LOCAL) {
 	define('MYSQL_HOST','');
-	define('MYSQL_DATABASE','');
 	define('MYSQL_USER','');
 	define('MYSQL_PASSWORD','');
+	define('MYSQL_DATABASE','');
 	define('MYSQL_TABLE','');
 } else {
 	define('MYSQL_HOST','');
-	define('MYSQL_DATABASE','');
 	define('MYSQL_USER','');
 	define('MYSQL_PASSWORD','');
+	define('MYSQL_DATABASE','');
 	define('MYSQL_TABLE','');
 }
-
-define('MYSQL_ALL', 'user_id, email, pageurl, firstname, lastname, access_level, last_login, date_joined, logins, pages');
 ?>
